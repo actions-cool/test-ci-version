7a7edc58a747532ae12c81b7963cd897dc87f408
xrkffgg <xrkffgg@vip.qq.com>
2021-01-29 17:20:07 +0800

    v1.1.0

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 
 async function run() {
   try {
-    console.log('v1.0.2');
+    console.log('v1.1.0');
   } catch (error) {
     core.setFailed(error.message);
   }
